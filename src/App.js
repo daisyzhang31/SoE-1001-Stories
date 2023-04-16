6fa5391130ab6e769144168f9c59f19050da2c3e
Junyue Zhang <daisyzhang0301@gmail.com>
2023-04-16 12:57:41 -0700

    test html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <div className="header__links header__links--left">
+          <div className="header__logo">Cohort Directory</div>
+        </div>
+        <nav className="header__links header__links--right">
+          <a href="/">Home</a>
+        </nav>
       </header>
-    </div>
+      <section className="student-list">
+        <div className="student">
+          <div className="student__content">
+            <img
+              className="student__image"
+              src="https://img.icons8.com/ios/100/000000/test-account.png"
+              alt="Test Student Account Image"
+            />
+          </div>
+          <h4 className="student__title">Student Name</h4>
+        </div>
+        <div className="student">
+          <div className="student__content">
+            <img
+              className="student__image"
+              src="https://img.icons8.com/ios/100/000000/test-account.png"
+              alt="Test Student Account Image"
+            />
+          </div>
+          <h4 className="student__title">Student Name</h4>
+        </div>
+        <div className="student">
+          <div className="student__content">
+            <img
+              className="student__image"
+              src="https://img.icons8.com/ios/100/000000/test-account.png"
+              alt="Test Student Account Image"
+            />
+          </div>
+          <h4 className="student__title">Student Name</h4>
+        </div>
+      </section>
+    </>
   );
 }
 
